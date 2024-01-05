489819f0dbc182fce700287cf9e7534965cb63af
AureLeeLi <aduynslaeger@gmail.com>
2024-01-05 10:44:58 +0100

    studio detail fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../routes/App.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export const Header = () => {
   return (
     <div className='header'>
-          <img src="img\logo.png" alt="" srcset=""/>
+          <Link to="/"><img src="img\logo.png" alt="" srcset=""/></Link>
     </div>
   )
 }
